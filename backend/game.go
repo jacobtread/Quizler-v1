d4269b53570390dacd0368fcb7b08c82e42ddb32
Jacobtread <jacobtread@gmail.com>
2022-02-14 22:05:22 +1300

    Create/get game, random ids and omit answer

--- backend/game.go
+++ backend/game.go
@@ -1,5 +1,11 @@
 package main
 
+import "math/rand"
+
+// Game A structure representing a game itself
+// Title the title of this game room
+// Questions The questions for this game
+// Players
 type Game struct {
 	Title     string         `json:"title"`
 	Questions []QuestionData `json:"questions"`
@@ -18,16 +24,62 @@ type Player struct {
 	Answers map[int16]string `json:"answers"`
 }
 
+// QuestionData A structure representing a question in the game
+// Title the title for this
+// Question the actual question that the player should be asked
+// Answers The list of possible answers for this question
+// Answer The correct answer for this question
 type QuestionData struct {
-	Id       int16    `json:"id"`
 	Title    string   `json:"title"`
 	Question string   `json:"question"`
 	Answers  []string `json:"answers"`
-	Answer   int16    `json:"answer"`
+	Answer   int16    `json:"-"`
 }
 
+// UniqueIdLength The length in chars that should be used for generating unique game ids
+const UniqueIdLength = 5
+
+// Games A map of unique game ids to the game objects
 var Games = map[string]Game{}
 
-func CreateGame(title string, questions []QuestionData) {
+// CreateRandomId Generates a random id string using the uppercase chars from A - F and
+// the numbers from 0 to 9
+func CreateRandomId() string {
+	chars := []rune("ABCDEF0123456789")
+	id := make([]rune, UniqueIdLength)
+	for i := range id {
+		id[i] = chars[rand.Intn(len(chars))]
+	}
+	return string(id)
+}
 
+// CreateUniqueId Generates random ids with CreateRandomId until a unique id that is not
+// present in the list of games is generated
+func CreateUniqueId() string {
+	for {
+		id := CreateRandomId()
+		_, contains := Games[id]
+		if !contains {
+			return id
+		}
+	}
+}
+
+// CreateGame creates a new game with the provided title and questions,
+// assigns it a unique id, stores it and returns the id and the game
+func CreateGame(title string, questions []QuestionData) (string, *Game) {
+	id := CreateUniqueId()
+	game := Game{Title: title, Questions: questions, Players: []Player{}}
+	Games[id] = game
+	return id, &game
+}
+
+// GetGame retrieves the game with the matching id from the games
+// list and returns it or nil if not present
+func GetGame(id string) *Game {
+	game, contains := Games[id]
+	if !contains {
+		return nil
+	}
+	return &game
 }
